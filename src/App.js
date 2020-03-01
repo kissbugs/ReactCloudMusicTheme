0e96473c7ab839415764052fc39928a8f66099a7
liwei <liwei@xinya.me>
2020-03-01 21:27:49 +0800

    bundle

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { hot } from 'react-hot-loader/root';
 import React from "react";
 import { Provider } from "react-redux"
 import store from "./store/index.js"
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import Routes from "./routes/index.jsx"
 
 import { GlobalStyle } from "./globalStyle.js";

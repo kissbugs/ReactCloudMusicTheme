3988fdc163e0b8c14fb59f515c721e7c2669ed45
liwei <liwei@xinya.me>
2020-03-08 12:14:34 +0800

    add album details page

--- src/application/Albums/index.jsx
+++ src/application/Albums/index.jsx
@@ -1,26 +1,22 @@
-import React, { memo, useEffect, useState } from "react";
+import React, { memo, useEffect, useCallback, useState } from "react";
 import * as S from "./style.js";
 
 const Albums = memo(({ ...props }) => {
-
-  const [back, setBack] = useState(true)
-  const handleBackClick = () => {
+  const [back, setBack] = useState(true);
+  const handleBackClick = useCallback(() => {
     setBack(false);
     setTimeout(() => {
       props.history.goBack();
     }, 500);
-  };
-  return (
-    <S.AlbumsContainer>
-      <div
-        // bounceInDown
-        className={`albums_container animated ${back ? "rollIn" : "rollOut"}`}
-      >
-        {/* <div className="back_title" onClick={() => props.history.goBack()}> */}
-        <div className="back_title" onClick={handleBackClick}>
-          <i className="iconfont icon_left">&#xe69d;</i>
-          <div className="title">歌单列表</div>
-        </div>
+  }, []);
+
+  const handleSongClick = useCallback(() => {
+    console.log("获取歌曲详情");
+  }, []);
+
+  const renderTopAlbumsBrief = () => {
+    return (
+      <div className="albums_main_description">
         <div className="album_main_picture">
           <img
             src="http://p1.music.126.net/VTW4vsN08vwL3uSQqPyHqg==/2002210674180199.jpg"
@@ -31,6 +27,48 @@ const Albums = memo(({ ...props }) => {
             本周封面：周深"
           </div>
         </div>
+      </div>
+    );
+  };
+
+  const renderAlbumsLists = () => {
+    return (
+      <div className="albums_lists_box">
+        <div className="play_list_entrance">
+          <div className="left_play_number">
+            <i className="iconfont icon_play">&#xe668;</i>
+            <span>15</span>
+            {/* <i className="iconfont icon_pause">&#xe607;</i> */}
+          </div>
+          {/* <div className="right_play_menu">
+            <i className="iconfont lef_menu">&#xe615;</i>
+          </div> */}
+        </div>
+        <ul>
+          <li onClick={handleSongClick}>
+            <img
+              src="http://p1.music.126.net/VTW4vsN08vwL3uSQqPyHqg==/2002210674180199.jpg"
+              alt=""
+            />
+            <div className="album_info">
+              <div className="title">好人一生平安</div>
+              <div className="description">做好人，做好事，好人好事</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  return (
+    <S.AlbumsContainer className={`animated ${back ? "rollIn" : "rollOut"}`}>
+      <div className="back_title" onClick={handleBackClick}>
+        <i className="iconfont icon_left">&#xe69d;</i>
+        <div className="title">歌单列表</div>
+      </div>
+      <div className="albums_container clear_scroll_bar">
+        {renderTopAlbumsBrief()}
+        {renderAlbumsLists()}
       </div>
     </S.AlbumsContainer>
   );
--- src/application/Albums/style.js
+++ src/application/Albums/style.js
@@ -15,33 +15,94 @@ export const AlbumsContainer = styled.div`
     max-width: 500px;
     margin: 0 auto;
   }
+  .back_title {
+    display: inline-flex;
+    align-items: center;
+    height: 50px;
+    cursor: pointer;
+    padding: 0 15px;
+    i {
+      color: #f0bf31;
+      font-size: 20px;
+    }
+    .title {
+      color: #fff;
+      padding-left: 10px;
+    }
+  }
   .albums_container {
-    margin: 15px 10px 10px;
-    .back_title {
-      display: inline-flex;
-      align-items: center;
-      cursor: pointer;
-      i {
-        color: #f0bf31;
-        font-size: 20px;
-      }
-      .title {
-        color: #fff;
-        padding-left: 10px;
+    height: calc(100vh - 50px);
+    overflow-y: scroll;
+    .albums_main_description {
+      margin: 0 15px 10px;
+      .album_main_picture {
+        padding: 20px 15px 15px;
+        img {
+          width: 200px;
+          border-radius: 10px;
+          margin: 0 auto;
+          display: block;
+        }
+        .album_description {
+          color: #fff;
+          padding: 20px 0;
+          line-height: 1.5em;
+        }
       }
     }
-    .album_main_picture {
-      padding: 20px 15px 15px;
-      img {
-        width: 200px;
-        border-radius: 10px;
-        margin: 0 auto;
-        display: block;
+    .albums_lists_box {
+      margin: 0 15px;
+      .play_list_entrance {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .left_play_number {
+          display: flex;
+          align-items: center;
+          .icon_play, .icon_pause  {
+            color: #f0bf31;
+            font-size: 30px;
+          }
+          span {
+            color: #828282;
+            padding-left: 10px;
+          }
+        }
+        .right_play_menu {
+          .lef_menu {
+            color: #f0bf31;
+            font-size: 30px;
+          }
+        }
       }
-      .album_description {
-        color: #fff;
-        padding: 20px 0;
-        line-height: 1.5em;
+      ul {
+        padding: 15px 0;
+        li {
+          display: flex;
+          align-items: center;
+          margin-bottom: 15px;
+          border-bottom: 1px dashed #5d5d5d;
+          padding-bottom: 15px;
+          &:last-child {
+            border-bottom: none;
+            margin-bottom: 0;
+          }
+          img {
+            width: 50px;
+            border-radius: 8px;
+          }
+          .album_info {
+            padding-left: 10px;
+            .title {
+              color: #fff;
+            }
+            .description {
+              color: #828282;
+              font-size: 12px;
+              padding-top: 10px;
+            }
+          }
+        }
       }
     }
   }
